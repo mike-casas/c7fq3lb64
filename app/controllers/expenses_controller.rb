c0d0af8055bdebf4c11822526b93c849fac7c1bf
Miguel Casas <miguel@kommit.co>
2016-03-15 20:00:16 -0500

    change query

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,7 +1,7 @@
 class ExpensesController < ApplicationController
   def index
-    if params[:category_id] != "" ||  params[:concept]!=""
-      @expenses = Expense.where("concept LIKE ?", "%#{params[:concept]}%")
+    if params[:category_id] != "" &&  params[:concept]!=""
+      @expenses = Expense.where("category_id= ? AND concept LIKE ?", params[:category_id],"%#{params[:concept]}%")
     else
       @expenses = Expense.order("date DESC")
     end
