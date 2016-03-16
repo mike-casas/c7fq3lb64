985a3b3e70af93b9e6365ec13d6d405758ce8331
Miguel Casas <miguel@kommit.co>
2016-03-15 19:43:56 -0500

    filter query where

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,9 @@
 class ExpensesController < ApplicationController
   def index
-     @expenses = Expense.order("date DESC")
+    if params[:category_id] != "" ||  params[:concept]!=""
+      @expenses = Expense.where("concept LIKE ?", "%#{params[:concept]}%")
+    else
+      @expenses = Expense.order("date DESC")
+    end
   end
 end
