a974dfeb9e7a5f76acdd02103ca1e2a73922855d
Miguel Casas <miguel@kommit.co>
2016-03-20 19:20:08 -0500

    add devise

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,15 +1,17 @@
 class ExpensesController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     if params[:category_id].present? &&  params[:concept].present?
-      @expenses = Expense.where("category_id= ? AND concept LIKE ?", params[:category_id],"%#{params[:concept]}%")
+      @expenses = Expense.where("category_id= ? AND concept LIKE ? AND user_id= ?", params[:category_id],"%#{params[:concept]}%", current_user)
     else
       if params[:category_id].present?
-        @expenses = Expense.where("category_id= ?", params[:category_id])
+        @expenses = Expense.where("category_id=? AND user_id= ?", params[:category_id], current_user)
       else
         if params[:concept].present?
-          @expenses = Expense.where("concept LIKE ?","%#{params[:concept]}%")
+          @expenses = Expense.where("concept LIKE ? AND user_id= ?","%#{params[:concept]}%", current_user)
           else
-            @expenses = Expense.order("date DESC")
+            @expenses = Expense.order("date DESC").where("user_id=?", current_user)
         end
       end
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'expenses#index'
-  
   resources :expenses, only: [:index]
 
   # The priority is based upon order of creation: first created -> highest priority.
